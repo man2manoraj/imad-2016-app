4e39d237df2be5c42cbc08977101e4fe8ea3147e
man2manoraj <man2manoraj@gmail.com>
2016-09-23 22:10:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req,res) {
 });
 
 app.get('/article-two',function(req,res) {
-   res.send('Article twoe is requested and will be servred here'); 
+   res.send('Article two is requested and will be servred here'); 
 });
 
 app.get('/article-three',function(req,res) {
